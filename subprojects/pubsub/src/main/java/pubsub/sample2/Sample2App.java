d8fbb74978276351a30e9c9857aec0640e1eab02
alan.bae <alan.bae@kakaopaycorp.com>
2023-12-15 14:18:47 +0900

    미니게임

--- subprojects/pubsub/src/main/java/pubsub/sample2/Sample2App.java
+++ subprojects/pubsub/src/main/java/pubsub/sample2/Sample2App.java
@@ -3,24 +3,23 @@ package pubsub.sample2;
 public class Sample2App {
 
     public static void main(String[] args) throws InterruptedException {
-        var th1 = new TestThread();
+        var th1 = new TestThread("test-thread-1");
         th1.start();
 
         System.out.println("Thread.currentThread(): " + Thread.currentThread());
         System.out.println("th1.current(): " + th1.current());
+
         System.out.println("===================================");
 
-//        Thread.sleep(5000);
-        System.out.println("init: " + th1.isInterrupted());
+        // false : TERMIN
+        System.out.println("init: " + th1.isInterrupted() + " : " + th1.getState());
 
+        // true : TER
         th1.interrupt();
-        System.out.println("th1.interrupt(): " + th1.isInterrupted());
+        System.out.println("th1.interrupt(): " + th1.isInterrupted() + " : " + th1.getState());
 
+        // false : TER
         Thread.interrupted();
-        System.out.println("Thread.interrupted(): " + th1.isInterrupted());
-
-//        th1.dointerrupted();
-//        System.out.println("th1.dointerrupted(): " + th1.isInterrupted());
-
+        System.out.println("Thread.interrupted(): " + th1.isInterrupted() + " : " + th1.getState());
     }
 }
--- subprojects/pubsub/src/main/java/pubsub/sample2/TestThread.java
+++ subprojects/pubsub/src/main/java/pubsub/sample2/TestThread.java
@@ -2,6 +2,10 @@ package pubsub.sample2;
 
 public class TestThread extends Thread {
 
+    public TestThread(String name) {
+        super(name);
+    }
+
     @Override
     public void run() {
         System.out.println("run: " + Thread.currentThread());
@@ -9,15 +13,16 @@ public class TestThread extends Thread {
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
-            System.out.println("catch: " + interrupted());
+            System.out.println("run catch: " + isInterrupted());
+            interrupted();
         }
+
+        interrupted();
+        System.out.println("run2: " + Thread.currentThread());
     }
 
     public Thread current() {
         return Thread.currentThread();
     }
 
-    public boolean dointerrupted() {
-        return Thread.interrupted();
-    }
 }
